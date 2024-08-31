c9abed425c4240572df82dbe0f5d319cbf710d52
mvp <mvp@users.noreply.github.com>
2024-08-31 16:57:18 -0700

    Release version 2.6.0 as stable brew tap

--- Formula/uhubctl.rb
+++ Formula/uhubctl.rb
@@ -2,8 +2,8 @@ class Uhubctl < Formula
   desc "USB hub per-port power control"
   homepage "https://github.com/mvp/uhubctl"
   head "https://github.com/mvp/uhubctl.git"
-  url "https://github.com/mvp/uhubctl/archive/v2.5.0.tar.gz"
-  sha256 "d4452252f7862f7a45dd9c62f2ea7cd3a57ab5f5ab0e54a857d4c695699bbba3"
+  url "https://github.com/mvp/uhubctl/archive/v2.6.0.tar.gz"
+  sha256 "56ca15ddf96d39ab0bf8ee12d3daca13cea45af01bcd5a9732ffcc01664fdfa2"
   license "GPL-2.0"
 
   depends_on "libusb"
