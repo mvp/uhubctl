a10d3534cfdf17eeb1544875dd003e0caaad8195
Tormod Volden <debian.tormod@gmail.com>
2024-11-24 13:18:22 +0100

    (fixup) allocate dev list dynamically

--- uhubctl.c
+++ uhubctl.c
@@ -1116,7 +1116,6 @@ int main(int argc, char *argv[])
   (defined(__gnu_linux__) || defined(__linux__))
     int sys_fd;
     libusb_device_handle *sys_devh = NULL;
-    libusb_device *dev_list[2];
 #endif
 
     for (;;) {
@@ -1250,9 +1249,13 @@ int main(int argc, char *argv[])
             rc = 1;
             goto cleanup;
         }
-        dev_list[0] = libusb_get_device(sys_devh);
-        dev_list[1] = NULL;
-        usb_devs = dev_list;
+        usb_devs = calloc(2, sizeof *usb_devs);
+        if (!usb_devs) {
+            fprintf(stderr, "Out of memory\n");
+            rc = 1;
+            goto cleanup;
+        }
+        usb_devs[0] = libusb_get_device(sys_devh);
     } else {
         rc = libusb_get_device_list(NULL, &usb_devs);
     }
@@ -1370,6 +1373,7 @@ cleanup:
         if (sys_devh)
             libusb_close(sys_devh);
         close(sys_fd);
+        free(usb_devs);
     } else if (usb_devs)
         libusb_free_device_list(usb_devs, 1);
 #else
