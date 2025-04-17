d00a167ab4a643313f92653769644fe7dd2bbc5c
Vadim Mikhailov <mvp@users.noreply.github.com>
2025-04-17 13:35:31 -0700

    homebrew audit insists on checksum before head

--- Formula/uhubctl.rb
+++ Formula/uhubctl.rb
@@ -2,8 +2,8 @@ class Uhubctl < Formula
   desc "USB hub per-port power control"
   homepage "https://github.com/mvp/uhubctl"
   url "https://github.com/mvp/uhubctl/archive/refs/tags/v2.6.0.tar.gz"
-  head "https://github.com/mvp/uhubctl.git", branch: "master"
   sha256 "56ca15ddf96d39ab0bf8ee12d3daca13cea45af01bcd5a9732ffcc01664fdfa2"
+  head "https://github.com/mvp/uhubctl.git", branch: "master"
   license "GPL-2.0-only"
 
   depends_on "pkg-config" => :build
