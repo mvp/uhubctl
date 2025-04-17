65f6d48cd1825c41c05a7a784e05100734835446
Vadim Mikhailov <mvp@users.noreply.github.com>
2025-04-17 13:44:56 -0700

    homebrew audit one more fix

--- Formula/uhubctl.rb
+++ Formula/uhubctl.rb
@@ -1,9 +1,9 @@
 class Uhubctl < Formula
   desc "USB hub per-port power control"
   homepage "https://github.com/mvp/uhubctl"
-  license "GPL-2.0-only"
   url "https://github.com/mvp/uhubctl/archive/refs/tags/v2.6.0.tar.gz"
   sha256 "56ca15ddf96d39ab0bf8ee12d3daca13cea45af01bcd5a9732ffcc01664fdfa2"
+  license "GPL-2.0-only"
   head "https://github.com/mvp/uhubctl.git", branch: "master"
 
   depends_on "pkg-config" => :build
